9db30ff847a004f0df019742c3257d9d5132a6c5
Alex Domin <alexdomin707@gmail.com>
2022-04-29 16:45:44 -0400

    added routes to show articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles, only: [:show]
+  #get 'articles', to: 'articles#show_article'
+
+  #at some point, figure out why the above code did not work
+  #it seems to follow all of the correct formatting
+  #and when I show routes in terminal, it is recognized
+  #but following the URI in the browser leads to an error
+  #stating 'No route matches [GET] "/articles/14"'
+  #however, path is '/articles(.:format)'
+  #and controller action is 'articles#show_article'
 end
