2c18bacb00e09ae9123522fff28505ee9dd1b28b
Alex Domin <alexdomin707@gmail.com>
2022-05-11 08:26:25 -0400

    added edit article fucntion

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
@@ -25,5 +29,15 @@ class ArticlesController < ApplicationController
     # redirect_to '/articles/new'
     # using the above code redirects back to the exact same
     # form after an article has been successfully submitted
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully!"
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 end
