419e8a17cb8a2ea6b58d5318d9c478ea2c694224
Alex Domin <alexdomin707@gmail.com>
2022-05-12 09:03:30 -0400

    added links to alpha-blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
-      flash[:notice] = "Article was saved successfully!"
+      flash[:notice] = "#{@article.title} was saved successfully!"
       redirect_to @article
     else
       render 'new'
@@ -34,7 +34,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     if @article.update(params.require(:article).permit(:title, :description))
-      flash[:notice] = "Article was updated successfully!"
+      flash[:notice] = "#{@article.title} was updated successfully!"
       redirect_to @article
     else
       render 'edit'
@@ -44,7 +44,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     if @article.destroy
-      flash[:notice] = "Article was successfully deleted!"
+      flash[:notice] = "#{@article.title} was successfully deleted!"
       redirect_to articles_path
     end
   end
