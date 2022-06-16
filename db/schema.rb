bc3db4c2ca12d4dabd8d2e09feec75beb62f4aa6
Alex Domin <alexdomin707@gmail.com>
2022-06-16 14:07:50 -0400

    added user validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_15_230141) do
+ActiveRecord::Schema.define(version: 2022_06_16_175431) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
