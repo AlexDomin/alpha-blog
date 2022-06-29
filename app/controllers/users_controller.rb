9ab185005a5b419f8c6a936c4333216897eaae64
Alex Domin <alexdomin707@gmail.com>
2022-06-29 19:59:10 -0400

    committing view only version (no article partial) of the user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def index
+    @user = User.all
+  end
+
   def new
     @user = User.new
   end
