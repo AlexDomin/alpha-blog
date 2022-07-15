8677f0bf2bc3ada9f76a5ea15b0783a9bc08f441
Alex Domin <alexdomin707@gmail.com>
2022-07-14 23:55:03 -0400

    added functional testing for create category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,17 @@
 class CategoriesController < ApplicationController
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was created successfully."
+      redirect_to @category
+    else
+      render 'new'
+    end
   end
   
   def show
@@ -8,5 +19,11 @@ class CategoriesController < ApplicationController
 
   def index
   end
-  
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
