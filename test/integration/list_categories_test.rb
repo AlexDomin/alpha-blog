85359b4dcb3ec90cde2c047391b03682f6581779
Alex Domin <alexdomin707@gmail.com>
2022-07-17 10:12:54 -0400

    added category index integration test and category index view/controller front end functionality

---
+++ test/integration/list_categories_test.rb
@@ -0,0 +1,14 @@
+require "test_helper"
+
+class ListCategoriesTest < ActionDispatch::IntegrationTest
+  def setup
+    @category = Category.create(name: "Sports")
+    @category2 = Category.create(name: "Travel")
+  end
+
+  test "list categories and link category name to show page" do
+    get '/categories'
+    assert_select "a[href=?]", category_path(@category), text: @category.name
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+  end
+end
