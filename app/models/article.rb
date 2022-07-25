dd0cc2ecd7174b8a8bff5287eca04b6768b54e33
Alex Domin <alexdomin707@gmail.com>
2022-07-25 17:51:01 -0400

    added categories selection list to create new article view, whitelisted category id params on controller level

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true, length: { minimum: 6, maximum: 50 }, uniqueness: { message: " already exists." }
   validates :description, presence: true, length: { minimum: 10, maximum: 40000 }, uniqueness: { message: " already exists." }
 end
